d915b5e3b42619c3197a22c72bb72101a1630a35
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-02-02 13:15:31 -0800

    Update PhaseSolver.java

--- PhaseSolver.java
+++ PhaseSolver.java
@@ -36,20 +36,40 @@ public class PhraseSolver
       /* your code here - game logic */
       
       
-      currentPlayer ==1;  // set the current player to Player 1
+      currentPlayer += 1;  // set the current player to Player 1
           // prompt the current player for a guess
       System.out.print("Would you like to guess a letter (a) or the phrase (b)");
       String choice = input.nextLine();
       if (choice.equals("a"))
       {
         System.out.println("What letter would you like to choose?");
+        String letter = input.nextLine();
+        for (int i = 0; i < game.getPhrase().length(); i++){
+            if (letter.equals(game.getPhrase().substring(i, i + 1)))
+        {
+    
+        }
+        }
+      
       }
       else
       {
-        System.out.println("What do you think the phrase is?")
+        System.out.println("What do you think the phrase is?");
+        String phraseGuess = input.nextLine();
+        if (phraseGuess.equals(game.getPhrase()))
+        {
+          System.out.println("Good job you have correctly guessed the phrase!");
+        }
+        else
+        {
+          System.out.println("Unfortunately you are stupid so you incorrectly guessed the phrase, better luck next time");
+          System.out.println("L");
+        }
+     
       }
     
     
+      
     
     
     
